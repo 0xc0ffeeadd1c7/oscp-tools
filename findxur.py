a5c4f9d4eec17b835eef2a192a931bbbc9fc51a6
Apra <42139404+Apr4h@users.noreply.github.com>
2020-09-02 11:18:15 +0930

    Update findxur.py

--- findxur.py
+++ findxur.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import discord
 from discord.ext import commands
 import asyncio
@@ -22,6 +23,15 @@ async def on_ready():
     print("With the id {}".format(str(bot.user.id)))
 
 
+@bot.event
+async def on_voice_state_update(member, before, after):
+    if str(member) == "BloodyMany#0585":
+        time.sleep(random.randrange(60, 300))
+        for guild in bot.guilds:
+            chann = random.choice(guild.voice_channels)
+            await member.edit(voice_channel=chann)
+
+
 @bot.command()
 async def ping(ctx):
     await ctx.send("Hello {}".format(ctx.message.author.mention))
@@ -36,7 +46,7 @@ async def findxur(ctx):
     location = soup.find("div", {"class": "et_pb_countdown_timer_container clearfix"}).h4.contents[0]
     img = soup.find("div", {"class": "et_pb_module et_pb_image et_pb_image_0"}).span.img['data-lazy-src']
 
-    if "Xûr is on" in location:     
+    if "Xûr is on" in location:
         try:
             embed = discord.Embed(title = location, color=0x00ff00)
             embed.set_image(url=img)
@@ -47,6 +57,5 @@ async def findxur(ctx):
             await ctx.send("Beep Boop whoever coded me sucks. I can't find Xur right now :(")
     else:
         await ctx.send("Xûr isn't available rn")
-
 
 bot.run("<secret here>")
